7c0c807f822c52f772791c571884619ceb07e74e
Kyra Steenbock <kyra.m.steenbock@gmail.com>
2015-06-24 08:16:01 -0600

    fixed user languages bug

--- app/models/match.rb
+++ app/models/match.rb
@@ -2,7 +2,7 @@ class Match < ActiveRecord::Base
   belongs_to :first_accepter, class_name: :User, foreign_key: "first_accepter_id"
   belongs_to :second_accepter, class_name: :User, foreign_key: "second_accepter_id"
 
-  def secondary_acceptor
+  def secondary_accepter
     User.find(second_accepter_id).username
   end
 end
